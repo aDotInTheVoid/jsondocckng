3056a12141e067974f00f21645cd7bc3b1ff84f2
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-02-18 18:05:32 +0000

    lib: Dont needlessly regerate json

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-use std::process::Command;
+#![cfg(test)]
+
+use std::{io::BufReader, process::Command};
 
 use anyhow::{bail, Result};
 use camino::{Utf8Path, Utf8PathBuf};
 use fs_err as fs;
-use rustdoc_types::Crate;
+use rustdoc_types::{Crate, Item, Module};
 
 #[path = "../json_tests/tests.rs"]
 mod tests;
@@ -23,49 +25,74 @@ impl Version {
     }
 }
 
-// TODO: Cache output
 fn load_json(v: Version, file: &Utf8Path) -> Result<Crate> {
     let output_dir = Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("_output")
         .join(file)
         .join(v.specifier());
-
-    let mut cmd = Command::new("rustdoc");
-    cmd.arg(v.specifier())
-        .arg(Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(file))
-        .args(["--output-format", "json"])
-        .args(["-Z", "unstable-options"])
-        .arg("--output")
-        .arg(&output_dir);
-
-    let output = cmd.output()?;
-
-    if !output.status.success() {
-        // TODO: Use get_args() to get the command line arguments
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        let stdout = String::from_utf8_lossy(&output.stdout);
-
-        let args = cmd
-            .get_args()
-            .map(|s| s.to_string_lossy())
-            .collect::<Vec<_>>()
-            .join(" ");
-
-        bail!(
-            "`rustdoc {}` failed with status {}\n=== stderr ===\n{}\n=== stdout===\n{}\n===",
-            args,
-            output.status,
-            stderr,
-            stdout
-        );
-    }
-
+    let input_rs_file = Utf8PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(file);
     let fname = file.file_name().unwrap().replace(".rs", ".json");
 
-    let json = fs::File::open(output_dir.join(fname))?;
-    let krate: Crate = serde_json::from_reader(json)?;
+    let output_json_file = output_dir.join(fname);
+
+    let need_regen = match fs::metadata(&output_json_file) {
+        Err(_) => true, // File doesn't exist
+        Ok(json_meta) => {
+            let rs_meta = fs::metadata(&input_rs_file)?;
+            json_meta.modified()? < rs_meta.modified()?
+        }
+    };
+
+    if need_regen {
+        eprintln!("Regenerating {}", output_json_file);
+        let mut cmd = Command::new("rustdoc");
+        cmd.arg(v.specifier())
+            .arg(input_rs_file)
+            .args(["--output-format", "json"])
+            .args(["-Z", "unstable-options"])
+            .arg("--output")
+            .arg(&output_dir);
+
+        let output = cmd.output()?;
+
+        if !output.status.success() {
+            // TODO: Use get_args() to get the command line arguments
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            let stdout = String::from_utf8_lossy(&output.stdout);
+
+            let args = cmd
+                .get_args()
+                .map(|s| s.to_string_lossy())
+                .collect::<Vec<_>>()
+                .join(" ");
+
+            // TODO: Maybe invalidate json
+
+            bail!(
+                "`rustdoc {}` failed with status {}\n=== stderr ===\n{}\n=== stdout===\n{}\n===",
+                args,
+                output.status,
+                stderr,
+                stdout
+            );
+        }
+    } else {
+        eprintln!("Using cached {}", &output_json_file);
+    }
+    let json = fs::File::open(&output_json_file)?;
+    let krate: Crate = serde_json::from_reader(BufReader::new(json))?;
 
     return Ok(krate);
+}
+
+fn load_by_name<'a>(c: &'a Crate, m: &Module, name: &str) -> &'a Item {
+    for i in &m.items {
+        let i = &c.index[&i];
+        if i.name.as_deref() == Some(name) {
+            return &i;
+        }
+    }
+    panic!("No item named {}", name);
 }
 
 #[macro_export]
